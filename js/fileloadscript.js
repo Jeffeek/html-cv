b36ad510cabdd02ed1ceaf0612aa40e84f4786fc
Mikhail Ilyushin <ilyushin_misha@mail.ru>
2020-11-15 16:06:25 +0300

    Добавлена возможность спарсить json файл для отображения секций и содержимого

--- js/fileloadscript.js
+++ js/fileloadscript.js
@@ -22,15 +22,17 @@ function dropHandler(evt)
  
     fileReader.onload = (progressEvent) =>
     {
-        console.log("onload!");
-        var stringData = fileReader.result;
-        let object = JSON.parse(stringData);
-        console.log(object);
+        console.log("onload!");     
     }
  
     fileReader.onloadend = (progressEvent) =>
     {
         console.log("onloadend!");
+        var stringData = fileReader.result;
+        let object = JSON.parse(stringData);
+        console.log(object);
+        parseHeader(object);
+        parseSections(object);
     }
  
     fileReader.onerror = (progressEvent) =>
@@ -46,4 +48,145 @@ function dragoverHandler(evt)
     evt.stopPropagation();
     evt.preventDefault();
     evt.dataTransfer.dropEffect = 'copy';
+}
+
+function parseHeader(profileObject)
+{
+    let name = profileObject.fullName;
+    let position = profileObject.position;
+    let phone = profileObject.phone;
+    let email = profileObject.email;
+    let sex = profileObject.sex;
+    let age = profileObject.age;
+
+    let nameNode = getNameNode();
+    nameNode.textContent = name;
+
+    let positionNode = getPositionNode();
+    positionNode.textContent = position;
+
+    let phoneNode = getPhoneNode();
+    phoneNode.textContent = phone;
+
+    let emailNode = getEmailNode();
+    emailNode.textContent = email;
+}
+
+function parseSections(profileObject)
+{
+    let sections = profileObject.sections;
+    let sectionsWrapper = getSectionsNode();
+    for (let i = 0; i < sections.length; i++)
+    {
+        let section = createSection(sections[i]);
+        sectionsWrapper.appendChild(section);
+        bindArrows(section);
+    }
+}
+
+function getNameNode()
+{
+    return document.getElementById('name');
+}
+
+function getPositionNode()
+{
+    return document.getElementById('position');
+}
+
+function getContactsNode()
+{
+    return document.getElementById('contacts');
+}
+
+function getPhoneNode()
+{
+    return getContactsNode().children.item(0).children.item(1);
+}
+
+function getEmailNode()
+{
+    return getContactsNode().children.item(1).children.item(1);
+}
+
+function getSectionsNode()
+{
+    return document.getElementById('sections');
+}
+
+function createSection(sectionObject)
+{
+    let section = document.createElement('div');
+    section.classList.add('section');
+    let dropDown = document.createElement('div');
+    dropDown.classList.add('drop-down');
+    let dropDownHead = document.createElement('div');
+    dropDownHead.classList.add('drop-down-head');
+    let headText = document.createElement('span');
+    headText.textContent = sectionObject.title.toUpperCase();
+    let headArrow = document.createElement('span');
+    headArrow.classList.add('btn', 'glyph', 'arrow', 'icon-arrow-down')
+    dropDownHead.appendChild(headText);
+    dropDownHead.appendChild(headArrow);
+    let list = document.createElement('div');
+    list.classList.add('drop-down-list');
+    if (sectionObject.isOpen == false)
+    {
+        list.classList.add('hide')
+    }
+    for (let i = 0; i < sectionObject.items.length; i++)
+    {
+        let item = createItem(sectionObject.items[i]);
+        list.appendChild(item);
+    }
+    dropDown.appendChild(dropDownHead)
+    dropDown.appendChild(list);
+    section.appendChild(dropDown);
+
+    console.dir(section);
+
+    return section;
+}
+
+function createItem(item)
+{
+    let itemElem = document.createElement('div');
+    itemElem.classList.add('drop-down-item');
+    let title = document.createElement('span');
+    title.classList.add('text-bd-3');
+    title.textContent = item.title;
+    let subTitle = document.createElement('span');
+    subTitle.classList.add('text-lgt-blue');
+    subTitle.textContent = item.subTitle;
+    let date = document.createElement('span');
+    date.textContent = `${item.from} - ${item.to}`;
+    date.classList.add('text-lgt-gray');
+    let itemsList = document.createElement('ul');
+    itemsList.classList.add('list');
+    for (let i = 0; i < item.items.length; i++)
+    {
+        let listItem = document.createElement('li');
+        let listItemText = document.createElement('span');
+        listItemText.textContent = item.items[i];
+        listItem.appendChild(listItemText);
+        itemsList.appendChild(listItem);
+    }
+    itemElem.appendChild(title);
+    itemElem.appendChild(subTitle);
+    itemElem.appendChild(date);
+    itemElem.appendChild(itemsList);
+    return itemElem;
+}
+
+function bindArrows(section)
+{
+    let list = section.children.item(0).children.item(1);
+    let arrow = section.children.item(0).children.item(0).children.item(1);
+    section.onclick = function(event)
+    {
+        //тогглим скрытие/раскрытие списка
+        list.classList.toggle('hide');
+        //тогглим саму стрелочку
+        arrow.classList.toggle('open');
+    }  
 }
--- js/pagescripts.js
+++ js/pagescripts.js
@@ -1,7 +1,17 @@
-let menuElem = document.getElementById('sweeties');
-let titleElem = menuElem.querySelector('.title');
+//ищем нужное меню
+let menu = document.getElementById('drop-down-test');
+//находим ноду "головы"
+let head =  menu.getElementsByTagName('div')[0];
+//находим лист всех элементов в списке
+let list = menu.getElementsByTagName('div')[1];
+//находим стрелочку управления
+let arrow = head.getElementsByTagName('span')[1];
 
-titleElem.onclick = function()
+//событие на клик по стрелочке
+arrow.onclick = function()
 {
-    menuElem.classList.toggle('open');
+    //тогглим скрытие/раскрытие списка
+    list.classList.toggle('hide');
+    //тогглим саму стрелочку
+    arrow.classList.toggle('open');
 };
